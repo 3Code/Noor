3e87f5f1285952585b460248554c04732e514202
3Code <tri3code@gmail.com>
2016-07-30 14:05:16 +0700

    Add project files.

---
+++ Noor3/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Noor3.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Noor3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Noor3.MvcApplication" Language="C#" %>
